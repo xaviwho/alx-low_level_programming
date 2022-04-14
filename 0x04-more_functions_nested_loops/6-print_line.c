b9c8505f07e4f4cfbf61a7a35869a684fde2df46
Xavier Kanu <xavierkanu@yahoo.com>
2022-04-14 06:45:28 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_line - draw line
+ * @n : number of _ to be printed
+ * Return:void
+ */
+
+void print_line(int n)
+{
+
+	int i = 0;
+
+	while (i < n && n > 0)
+	{
+		_putchar('_');
+		i++;
+	}
+	_putchar('\n');
+}
+© 2022 GitHub, Inc.
+Terms
+Priv
